ad64f8734874180ff82c5ea1d318d08a5874dbd5
Miro Nieminen <miro.nieminen@futurice.com>
2016-03-12 00:43:48 +0200

    Show "meta"-section on EventDetail-view

--- app/components/calendar/EventDetail.js
+++ app/components/calendar/EventDetail.js
@@ -18,6 +18,8 @@ var {
 import Icon from 'react-native-vector-icons/Ionicons';
 import theme from '../../style/theme';
 
+import time from '../../utils/time';
+
 const styles = StyleSheet.create({
   detailEvent: {
     flex: 1,
@@ -48,18 +50,39 @@ const styles = StyleSheet.create({
 });
 
 const EventDetail = React.createClass({
-  onPressBack () {
+  onPressBack() {
     this.props.navigator.pop();
   },
 
+  getEventStatus(timepoint) {
+    if (timepoint.onGoing) {
+      return <Text>Käynnissä ny!</Text>;
+    } else if (timepoint.startsSoon) {
+      return <Text>Alkaa kohta!</Text>;
+    } else {
+      return null;
+    }
+  },
+
   render: function() {
+    // TODO: stylize the "meta-elements"
+
     const model = this.props.route.model;
+    const timepoint = time.formatEventTime(model.startTime, model.endTime, { formatLong: true });
 
     return <View style={styles.detailEvent}>
       <ScrollView>
-        <TouchableHighlight  underlayColor={theme.light}>
+        <TouchableHighlight underlayColor={theme.light}>
           <Image source={{ uri: model.coverImage }} style={styles.detailEventImg} />
         </TouchableHighlight>
+
+
+        <View>
+          <Text><Icon name='social-facebook' size={20} />Event</Text>
+          <Text><Icon name='android-calendar' size={15} /> {timepoint.date}</Text>
+          <Text><Icon name='android-time' size={15} /> {timepoint.time}</Text>
+          {this.getEventStatus(timepoint)}
+        </View>
 
         <View style={styles.content}>
           <Text style={styles.detailEventName}>{model.name}</Text>
