8e6a1d1d7f2af7e7a77f5b0057190b3b4f879f10
Miro Nieminen <miro.nieminen@futurice.com>
2016-03-12 00:09:19 +0200

    Implement features to time -module

--- app/utils/time.js
+++ app/utils/time.js
@@ -1,16 +1,48 @@
 import moment from 'moment';
 
-const BASIC_FORMAT = 'DD.MM HH:mm';
+const DAY_FORMAT = 'D.M.';
+const TIME_FORMAT = 'HH:mm';
 
+const TRESHOLD_FOR_STARTS_SOON = 120; // in minutes
 
-function format(time) {
-  if (!time) {
-    return '';
-  } else {
-    return moment(time).format(BASIC_FORMAT);
+function formatEventTime(startTime, endTime, opts) {
+  opts = opts || {};
+
+  let formatted = {
+    date: '',
+    time: '',
+    onGoing: false,
+    startsSoon: false
+  };
+
+  if (!startTime) {
+    return formatted;
   }
+
+  let startMoment = moment(startTime);
+  let endMoment = moment(endTime);
+
+  formatted.date = startMoment.format(DAY_FORMAT);
+  formatted.time = startMoment.format(TIME_FORMAT);
+
+  formatted.onGoing = eventIsOnGoing(startTime, endTime);
+  formatted.startsSoon = eventStartsSoon(startTime)
+
+  return formatted;
 }
 
+function eventIsOnGoing(startTime, endTime) {
+  let now = moment();
+  return moment(startTime).isBefore(now) && now.isBefore(moment(endTime));
+}
+
+function eventStartsSoon(startTime) {
+  return moment().diff(moment(startTime), 'minutes') <= TRESHOLD_FOR_STARTS_SOON;
+}
+
+
+
 export default {
-  format
+  formatEventTime,
+  eventIsOnGoing
 };
