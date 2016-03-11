044cde1eaa6a91c428b44681d81f3250d30785e1
Miro Nieminen <miro.nieminen@futurice.com>
2016-03-11 21:00:24 +0200

    Implement "listState" to Event-store, representing API-fetch progress
    (ping @anttti, does this make any sense)

--- app/actions/event.js
+++ app/actions/event.js
@@ -4,20 +4,32 @@ import ActionTypes from '../constants/ActionTypes';
 import api from '../services/api';
 
 const EVENT_SET = 'EVENT_SET';
+const EVENT_LIST_LOADING = 'EVENT_LIST_LOADING';
+const EVENT_LIST_LOADED = 'EVENT_LIST_LOADED';
+const EVENT_LIST_FAILED = 'EVENT_LIST_FAILED';
 
 
 const fetchEvents = () => {
   return (dispatch) => {
-    api.fetchModels('events').then(items => {
-      dispatch({
-        type: EVENT_SET,
-        events: items
-      });
-    });
+    dispatch({ type: EVENT_LIST_LOADING });
+
+    api.fetchModels('events')
+      .then(items => {
+        dispatch({
+          type: EVENT_SET,
+          events: items
+        });
+
+        dispatch({ type: EVENT_LIST_LOADED });
+      })
+      .catch(error => dispatch({ type: EVENT_LIST_FAILED }));
   }
 };
 
 export {
   EVENT_SET,
+  EVENT_LIST_LOADING,
+  EVENT_LIST_LOADED,
+  EVENT_LIST_FAILED,
   fetchEvents
 };
--- app/reducers/event.js
+++ app/reducers/event.js
@@ -2,11 +2,15 @@
 import Immutable from 'immutable';
 
 import {
-  EVENT_SET
+  EVENT_SET,
+  EVENT_LIST_LOADING,
+  EVENT_LIST_LOADED,
+  EVENT_LIST_FAILED
 } from '../actions/event';
 
 const initialState = Immutable.fromJS({
-  list: []
+  list: [],
+  listState: 'none'
 });
 
 
@@ -14,6 +18,12 @@ export default function event(state = initialState, action) {
   switch (action.type) {
     case EVENT_SET:
       return state.set('list', Immutable.fromJS(action.events));
+    case EVENT_LIST_LOADING:
+      return state.set('listState', 'loading');
+    case EVENT_LIST_LOADED:
+      return state.set('listState', 'ready');
+    case EVENT_LIST_FAILED:
+      return state.set('listState', 'failed');
     default:
       return state;
   }
