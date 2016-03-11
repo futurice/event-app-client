d964b09bf7d0458218a301f84ad2aff8ee7d7275
Antti Mattila <antti.mattila@futurice.com>
2016-03-11 17:45:04 +0200

    Add logger

--- app/containers/RootView.js
+++ app/containers/RootView.js
@@ -4,10 +4,15 @@ import React, { Component } from "react-native";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import createLoggerMiddleware from "redux-logger";
+import loggerConfig from "../utils/loggerConfig";
 import * as reducers from "../reducers";
 import MainView from "./MainView";
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+    thunk,
+    createLoggerMiddleware(loggerConfig)
+)(createStore);
 const reducer = combineReducers(reducers);
 const store = createStoreWithMiddleware(reducer);
 
