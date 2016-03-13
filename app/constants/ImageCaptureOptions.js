12a432f19dbedabcaa8a63dc57f0c766b39bf274
Antti Mattila <antti.mattila@futurice.com>
2016-03-13 09:46:10 +0200

    Up the quality

--- app/constants/ImageCaptureOptions.js
+++ app/constants/ImageCaptureOptions.js
@@ -12,7 +12,7 @@ export default {
   maxHeight: 500,
   aspectX: 2, // android only - aspectX:aspectY, the cropping image's ratio of width to height
   aspectY: 1, // android only - aspectX:aspectY, the cropping image's ratio of width to height
-  quality: 0.3, // 0 to 1, photos only
+  quality: 0.8, // 0 to 1, photos only
   angle: 0,
   allowsEditing: false, // Built in functionality to resize/reposition the image after selection
   noData: false, // photos only - disables the base64 `data` field from being generated (greatly improves performance on large photos)
