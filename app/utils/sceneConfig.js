88810f944e25a4b2291dc3bd0af9822ef89c3be1
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2016-03-13 18:36:43 +0200

    Fix scene config on different platforms

--- app/utils/sceneConfig.js
+++ app/utils/sceneConfig.js
@@ -1,20 +1,21 @@
 import React, { Navigator } from 'react-native';
 import { connect } from 'react-redux';
 const SCREEN_WIDTH = require('Dimensions').get('window').width;
+const { Platform } = React;
 
-/**
- * Overwrite the default navigator scene config.
- * to use a wider area for back swiping.
- */
-const sceneConfig = {
-  ...Navigator.SceneConfigs.FadeAndroid,
-  ...Navigator.SceneConfigs.FloatFromRight,
-  gestures: {
-    pop: {
-      ...Navigator.SceneConfigs.FloatFromRight.gestures.pop,
-      edgeHitWidth: SCREEN_WIDTH / 2,
+let sceneConfig;
+if (Platform.OS === 'ios') {
+  sceneConfig = {
+    ...Navigator.SceneConfigs.FloatFromRight,
+    gestures: {
+      pop: {
+        ...Navigator.SceneConfigs.FloatFromRight.gestures.pop,
+        edgeHitWidth: SCREEN_WIDTH / 2,
+      },
     },
-  },
-};
+  }
+} else {
+  sceneConfig = Navigator.SceneConfigs.FadeAndroid;
+}
 
 export default sceneConfig;
