bd42d7b55715d4b6d995f8ad425cf675fcacf72b
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2016-03-13 03:18:44 +0200

    Separated FeedView into android and ios versions. Added top navigator for ios.

---
+++ app/containers/FeedView.ios.js
@@ -0,0 +1,66 @@
+'use strict';
+
+import React, {
+  Component,
+  Navigator,
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight
+} from 'react-native';
+import { connect } from 'react-redux';
+import FeedList from '../components/feed/FeedList';
+import NavRouteMapper from '../components/common/navbarRouteMapper';
+const theme = require('../style/theme');
+
+
+const styles = StyleSheet.create({
+  navigator: {
+    paddingTop: 62
+  },
+  navbar: {
+    backgroundColor: theme.primary,
+    height: 62,
+    paddingBottom: 5,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+});
+
+var FeedView = React.createClass({
+  renderScene(route, navigator) {
+    if (route.component) {
+      const Component = route.component;
+      return <Component navigator={navigator} route={route} {...this.props} />
+    }
+  },
+
+  render() {
+    return (
+      <Navigator
+        style={styles.navigator}
+        navigationBar={
+          <Navigator.NavigationBar
+            style={styles.navbar}
+            routeMapper={NavRouteMapper} />
+        }
+        initialRoute={{
+          component: FeedList,
+          name: 'Feed'
+        }}
+        renderScene={this.renderScene}
+        configureScene={() => ({
+          ...Navigator.SceneConfigs.FloatFromRight
+        })}
+      />
+    );
+  }
+});
+
+
+const select = store => {
+    return {
+    }
+};
+
+export default connect(select)(FeedView);
