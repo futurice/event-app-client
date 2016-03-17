6f4df9d98a2a0d31c2ceb1f26c776671308bfc96
Miro Nieminen <miro.nieminen@futurice.com>
2016-03-17 22:06:48 +0200

    Housecleaning: sort functions on API based on their publicness

--- app/services/api.js
+++ app/services/api.js
@@ -7,6 +7,56 @@ import {version as VERSION_NUMBER} from '../../package.json';
 const USER_UUID = DeviceInfo.getUniqueID();
 const API_TOKEN = 'hessu'; // TODO implement build step & get this from env config or some other magical solution
 
+
+// # Exported functions --------------------------------------------------
+//
+const fetchModels = modelType => {
+  const url = Endpoints.urls[modelType];
+  return wapuFetch(url)
+  .then(response => response.json())
+  .then(response => {
+    return AsyncStorage.setItem(url, JSON.stringify(response)).then(() => response);
+  })
+  .catch((error) => {
+    console.log('Error catched on API-fetch', error);
+    return AsyncStorage.getItem(url)
+    .then((value) => {
+      if (value != null) {
+        return Promise.resolve(JSON.parse(value));
+      } else {
+        return Promise.reject(null);
+      }
+    });
+  });
+};
+
+const postAction = (params, location) => {
+  let payload = Object.assign({}, params, {
+    user: DeviceInfo.getUniqueID(),
+    location: location
+  });
+  return _post(Endpoints.urls.action, payload);
+};
+
+const putUser = payload => {
+  return _put(Endpoints.urls.user(payload.uuid), payload);
+};
+
+const getUser = uuid => {
+  return wapuFetch(Endpoints.urls.user(uuid))
+    .then(checkResponseStatus)
+    .then(response => response.json());
+};
+
+const deleteFeedItem = item => {
+  return _delete(Endpoints.urls.feedItem(item.id));
+};
+
+
+
+// # Internal functions --------------------------------------------------
+//
+
 // Our own wrapper for fetch. Logs the request, adds required version headers, etc.
 // Instead of using fetch directly, always use this.
 const wapuFetch = (url, opts) => {
@@ -30,6 +80,7 @@ const checkResponseStatus = response => {
   if (response.status >= 200 && response.status < 300) {
     return response;
   } else {
+    console.log('Error catched', response.statusText);
     const error = new Error(response.statusText);
     error.response = response;
     throw error;
@@ -69,48 +120,6 @@ const _delete = (url, body) => {
   }).then(checkResponseStatus);
 };
 
-const fetchModels = modelType => {
-  const url = Endpoints.urls[modelType];
-  return wapuFetch(url)
-  .then(response => response.json())
-  .then(response => {
-    return AsyncStorage.setItem(url, JSON.stringify(response)).then(() => response);
-  })
-  .catch((error) => {
-    console.log('Error catched on API-fetch', error);
-    return AsyncStorage.getItem(url)
-    .then((value) => {
-      if (value != null) {
-        return Promise.resolve(JSON.parse(value));
-      } else {
-        return Promise.reject(null);
-      }
-
-    });
-  });
-};
-
-const postAction = (payload, location) => {
-  const finalPayload = Object.assign({}, payload, {
-    user: DeviceInfo.getUniqueID(),
-    location: location
-  });
-  return _post(Endpoints.urls.action, finalPayload);
-};
-
-const putUser = payload => {
-  return _put(Endpoints.urls.user(payload.uuid), payload);
-};
-
-const getUser = uuid => {
-  return wapuFetch(Endpoints.urls.user(uuid))
-    .then(checkResponseStatus)
-    .then(response => response.json());
-};
-
-const deleteFeedItem = item => {
-  return _delete(Endpoints.urls.feedItem(item.id));
-};
 
 export default {
   deleteFeedItem,
