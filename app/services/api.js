56bdeccb23f213a9d6141e1ade94680cf9c55695
Antti Mattila <antti.mattila@futurice.com>
2016-03-18 21:34:18 +0200

    Fix error handling

--- app/services/api.js
+++ app/services/api.js
@@ -111,10 +111,13 @@ const checkResponseStatus = response => {
   if (response.status >= 200 && response.status < 300) {
     return response;
   } else {
-    console.log('Error catched', response.statusText);
-    const error = new Error(response.statusText);
-    error.response = response;
-    throw error;
+    return response.json()
+      .then(res => {
+        console.log('Error catched', response.statusText);
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      })
   }
 };
 
