1b1b981529edcadc34bb1c81e52286eb9f6e45b1
Pasi Lampinen <pasi.lampinen@futurice.com>
2016-03-22 17:06:05 +0200

    Android specific Button and Notification

---
+++ app/components/common/Button.android.js
@@ -0,0 +1,82 @@
+'use strict';
+
+import React, {
+  View,
+  TouchableNativeFeedback,
+  Text,
+  StyleSheet,
+  PropTypes
+} from 'react-native';
+
+var Button = React.createClass({
+  propTypes: Object.assign({},
+    {
+      textStyle: Text.propTypes.style,
+      disabledStyle: Text.propTypes.style,
+      children: PropTypes.string.isRequired,
+      isDisabled: PropTypes.bool,
+      onPress: PropTypes.func,
+      onPressIn: PropTypes.func,
+      onPressOut: PropTypes.func
+    },
+  ),
+
+  _renderInnerText: function () {
+    return (
+      <Text style={[styles.textButton, this.props.textStyle]}>
+        {this.props.children.toUpperCase()}
+      </Text>
+    );
+  },
+
+  render: function () {
+    if (this.props.isDisabled === true) {
+      return (
+        <View style={[styles.button, this.props.style, (this.props.disabledStyle || styles.opacity)]}>
+          <Text style={{textAlign:'center'}}>{this._renderInnerText()}</Text>
+        </View>
+      );
+    } else {
+      const touchableProps = {
+        onPress: this.props.onPress,
+        onPressIn: this.props.onPressIn,
+        onPressOut: this.props.onPressOut,
+      };
+      return (
+        <View style={[styles.button, this.props.style]}>
+        <TouchableNativeFeedback {...touchableProps}
+          delayPressIn={0}
+          background={TouchableNativeFeedback.SelectableBackground()}
+        >
+          <View style={{flex:1, justifyContent:'center', height:40}}>
+            {this._renderInnerText()}
+          </View>
+        </TouchableNativeFeedback>
+        </View>
+      );
+    }
+  }
+});
+
+const styles = StyleSheet.create({
+  button: {
+    flex:1,
+    height: 40,
+    justifyContent: 'center',
+    backgroundColor: '#50E3C2',
+    borderRadius: 2,
+    paddingLeft: 0,
+    paddingRight: 0
+  },
+  textButton: {
+    alignSelf: 'center',
+    fontSize: 13,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  opacity: {
+    opacity: 0.5
+  }
+});
+
+export default Button;
---
+++ app/components/common/Notification.android.js
@@ -0,0 +1,65 @@
+import React, {
+  Component,
+  PropTypes,
+  View,
+  ToastAndroid,
+} from 'react-native';
+
+class Notification extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    visible: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    visible: true,
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    if (this.props.visible) {
+      this._show()
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.visible && !this.props.visible && nextProps.children !== '') {
+      this._show();
+    } else {
+      if (!nextProps.visible && this.props.visible) {
+        this._hide();
+      }
+    }
+  }
+
+  shouldComponentUpdate(nextProps) {
+    if ( this.props.visible !== nextProps.visible) {
+      return true;
+    }
+    return false;
+  }
+
+  _show(){
+    setTimeout( () => {
+      console.log(this.props.children)
+
+      ToastAndroid.show(this.props.children, ToastAndroid.LONG)
+
+    }, 300)
+  }
+
+  _hide(){
+    // hidden
+  }
+
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+export default Notification;
