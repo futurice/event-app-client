c46c1fbcdcfcea826cc9f6a940b6c0321df867af
Pasi Lampinen <pasi.lampinen@futurice.com>
2016-03-22 21:09:28 +0200

    Added Licences link, changed icons

--- app/constants/Links.js
+++ app/constants/Links.js
@@ -2,7 +2,8 @@ const ROOT_URL = 'http://wappu.futurice.com';
 
 const Terms = [
   {title: 'Terms of Service', link: `${ROOT_URL}/terms`, icon: 'info-outline'},
-  {title: 'Privacy', link: `${ROOT_URL}/privacy`, icon: 'help-outline'},
+  {title: 'Privacy', link: `${ROOT_URL}/privacy`, icon: 'lock-outline'},
+  {title: 'Licences', link: `${ROOT_URL}/licences`, icon: 'help-outline'},
   {title: 'Wanna work at Futurice?',
     link: 'http://futurice.com/careers?utm_source=wappuapp&utm_medium=app&utm_campaign=wappu2016',
     icon: 'send'}
