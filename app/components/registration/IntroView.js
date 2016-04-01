93d330a455184cf112ef8399bb45654ec5753cd8
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2016-04-01 20:37:27 +0300

    Improve introview

--- app/components/registration/IntroView.js
+++ app/components/registration/IntroView.js
@@ -20,64 +20,63 @@ const IntroView = React.createClass({
     return (
       <View style={[styles.container, styles.modalBackgroundStyle]}>
         <Toolbar icon='' iconClick={() => null} title='Introduction' />
-
-        <Image
-          style={styles.imageContainer}
-          source={{uri: 'https://storage.googleapis.com/wappuapp/assets/intro-background.jpg'}}
-        >
           <ScrollView style={{flex:1, width: null, height: null}}>
-            <View style={styles.container}>
+            <View style={[styles.container, styles.contentContainer]}>
+              <Text style={styles.header}>
+                How to Whappu
+              </Text>
 
-              <View style={styles.row}>
+              <View style={[styles.row, {paddingTop: 30}]}>
                 <View style={styles.rowIconContainer}>
-                  <Icon name='android-chat' style={[styles.rowIcon, {color: theme.secondary}]} />
-                </View>
-
-                <View style={styles.rowNumberContainer}>
-                  <Text style={styles.rowNumberText}>1.</Text>
+                  <Icon name='android-chat' style={[styles.rowIcon, {color: theme.light}]} />
                 </View>
 
                 <View style={styles.rowTextContainer}>
+                  <Text style={styles.rowTitle}>
+                    1. Earn points
+                  </Text>
                   <Text style={styles.rowText}>
-                    Be active in Whappu Buzz
+                    Guild with most Whappu points wins a juicy prize!
                   </Text>
                 </View>
               </View>
 
               <View style={styles.row}>
                 <View style={styles.rowIconContainer}>
-                  <Icon name='beer' style={[styles.rowIcon, {color: theme.primary}]} />
-                </View>
-
-                <View style={styles.rowNumberContainer}>
-                  <Text style={styles.rowNumberText}>2.</Text>
+                  <Icon name='android-bar' style={[styles.rowIcon, {color: theme.light}]} />
                 </View>
 
                 <View style={styles.rowTextContainer}>
+                  <Text style={styles.rowTitle}>
+                    2. Enjoy sima
+                  </Text>
                   <Text style={styles.rowText}>
-                    Enjoy sima
+                    Because otherwise you might get thirsty.
                   </Text>
                 </View>
               </View>
 
               <View style={styles.row}>
                 <View style={styles.rowIconContainer}>
-                  <Icon name='trophy' style={[styles.rowIcon, {color: '#FFCC03'}]} />
-                </View>
-
-                <View style={styles.rowNumberContainer}>
-                  <Text style={styles.rowNumberText}>3.</Text>
+                  <Icon name='trophy' style={[styles.rowIcon, {color: theme.light}]} />
                 </View>
 
                 <View style={styles.rowTextContainer}>
+                  <Text style={styles.rowTitle}>
+                    3. Winner takes it all
+                  </Text>
                   <Text style={styles.rowText}>
-                    Guild with most Whappu points wins a juicy prize
+                    Competition ends at 12:00AM on 1st of May.
+                  </Text>
+
+                  <Text style={[styles.rowText, styles.rowSecondaryText]}>
+                    Winner will be
+                    announced later on the day.
                   </Text>
                 </View>
               </View>
             </View>
           </ScrollView>
-        </Image>
 
         <View style={styles.bottomButtons}>
           <Button
@@ -95,11 +94,18 @@ const IntroView = React.createClass({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingBottom: 50
+    paddingBottom: 30
   },
   innerContainer: {
     flex: 1,
     paddingTop: IOS ? 15 : 15,
+  },
+  header: {
+    fontWeight: 'bold',
+    color: theme.secondary,
+    paddingTop: 30,
+    paddingLeft: 30,
+    fontSize: 28
   },
   imageContainer: {
     flex: 1,
@@ -108,42 +114,64 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
   },
   row: {
-    paddingTop: 60,
     padding: 20,
+    paddingBottom: 25,
     flex: 1,
-    flexDirection:'row',
-    height: 100
+    flexDirection:'row'
   },
   rowNumberContainer: {
     paddingLeft: 10,
+    paddingTop:6,
+    borderWidth:2,
+    borderColor:theme.secondary,
+    borderRadius:25,
+    width:50,
+    height:50,
     paddingRight: 10,
     top: 2
   },
   rowNumberText: {
     fontSize: 28,
-    color: theme.white,
+    color: theme.secondary,
+    textAlign: 'center',
     backgroundColor: 'rgba(0,0,0,0)',
     fontWeight: 'bold'
   },
   rowIconContainer: {
-    width: 60
+    width: 50,
+    height:50,
+    backgroundColor:theme.secondary,
+    borderRadius:25,
+    marginRight:5,
+    marginLeft:10,
+    justifyContent:'center'
   },
   rowIcon: {
-    paddingLeft: 10,
+    textAlign:'center',
     color: theme.white,
     backgroundColor: 'rgba(0,0,0,0)',
-    fontSize: 44,
+    fontSize: 24,
     top: 3
   },
   rowTextContainer: {
     flex: 1,
-    top: 6
+    top: 2,
+    marginLeft: 20,
+  },
+  rowTitle:{
+    color:theme.secondary,
+    fontWeight:'bold',
+    fontSize: 18,
+    marginBottom: 8,
   },
   rowText: {
-    color: theme.white,
+    color: '#555',
     backgroundColor: 'rgba(0,0,0,0)',
     fontSize: 16,
-    fontWeight: 'bold'
+    fontWeight: 'normal'
+  },
+  rowSecondaryText: {
+    paddingTop: 8
   },
   bottomButtons:{
     flex:1,
