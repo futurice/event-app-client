4bdcc9f4844f2bfa20339ec839b3f984aaa8318d
Pasi Lampinen <plam@fl027.local>
2017-09-07 12:46:15 +0300

    Update links with 2017 content

--- app/constants/Links.js
+++ app/constants/Links.js
@@ -2,15 +2,15 @@ const ROOT_URL = 'https://futurice.github.io/futubileet-site';
 
 
 import locationService from '../services/location';
-const EVENT_LOCATION = { latitude: 60.2875359, longitude: 25.0384261 };
+const EVENT_LOCATION = { latitude: 60.1756973, longitude: 24.9336035 };
 
 const Links = [
-  {title: 'Heureka Website', link: 'http://www.heureka.fi', icon: 'lightbulb-outline'},
+  {title: 'Finlandia Hall Website', link: 'https://www.finlandiatalo.fi/en', icon: 'account-balance'},
   {title: 'Directions to Heureka', link: locationService.getGeoUrl({ location: EVENT_LOCATION, locationName: 'Futubileet in Heureka' }), icon: 'directions' },
-  {title: 'Call Taxi Vantaa', link: 'tel://01007300', icon: 'directions-car', separatorAfter: true},
-  {title: 'Terms of Service', link: `${ROOT_URL}/terms.html`, icon: 'info', showInWebview: true},
-  {title: 'Privacy', link: `${ROOT_URL}/privacy.html`, icon: 'lock', showInWebview: true},
-  {title: 'Licenses', link: `${ROOT_URL}/licenses.html`, icon: 'help', showInWebview: true, last: true},
+  {title: 'Call Taxi Helsinki', link: 'tel://01000700', icon: 'directions-car', separatorAfter: false},
+  {title: 'Terms of Service', link: `${ROOT_URL}/terms.html`, icon: 'info', secondary: true, showInWebview: true},
+  {title: 'Privacy', link: `${ROOT_URL}/privacy.html`, icon: 'lock', secondary: true, showInWebview: true},
+  {title: 'Licenses', link: `${ROOT_URL}/licenses.html`, icon: 'help', secondary: true, showInWebview: true, last: true},
 
 ];
 
