6bf08e198e069774e8d77b066991e9d78c2682df
Pasi Lampinen <plam@fl027.local>
2017-09-07 12:53:19 +0300

    Background pattern component

---
+++ app/components/background/index.js
@@ -0,0 +1,45 @@
+import React, { PropTypes, Component } from 'react';
+import {
+  StyleSheet,
+  Dimensions,
+  Image
+} from 'react-native';
+
+const { height } = Dimensions.get('window');
+const styles = StyleSheet.create({
+  image: {
+    position: 'absolute',
+    height,
+    right: 0,
+    left: 0,
+    top: 0,
+    opacity: 1,
+    zIndex: 0,
+  }
+});
+
+const images = {
+  purple: require('../../../assets/patterns/pattern-purple.png'),
+  yellow: require('../../../assets/patterns/pattern-yellow.png')
+}
+
+class BackgroundLayer extends Component {
+  static propTypes = {
+    color: PropTypes.string
+  };
+
+  render() {
+
+    const { color } = this.props;
+
+    return (<Image
+      resizeMode={'repeat'}
+      source={images[color] || images.purple}
+      style={styles.image}
+      />
+    )
+  }
+
+};
+
+export default BackgroundLayer;
