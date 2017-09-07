51ad2812f83b39f6076d75ef0c87ed03d91d4e48
Pasi Lampinen <plam@fl027.local>
2017-09-07 12:47:07 +0300

    Update RANK view to 2017 designs

--- app/components/competition/LeaderboardEntry.js
+++ app/components/competition/LeaderboardEntry.js
@@ -3,19 +3,19 @@
 import React, { PropTypes } from 'react';
 import {
   View,
-  Text,
   Image,
   Platform,
   Dimensions,
   StyleSheet,
   LayoutAnimation
 } from 'react-native';
+import Text from '../Text';
 import Immutable from 'immutable';
 import theme from '../../style/theme';
 
 import TimerMixin from 'react-timer-mixin';
-
 const IOS = Platform.OS === 'ios';
+
 const LeaderboardEntry = React.createClass({
   mixins: [TimerMixin],
   propTypes: {
@@ -37,69 +37,54 @@ const LeaderboardEntry = React.createClass({
         return 'th';
     }
   },
-  componentWillMount() {
-    LayoutAnimation.spring();
-  },
-  getInitialState() {
-    return {
-      width: 25
-    }
-  },
   componentDidMount() {
     // Increase min width if the team has some points, so that if winner has
     // e.g. 40000, the team with 30 points does not have too short bar
-    const minWidth = this.props.team.get('score') > 10 ? 46 : (IOS ? 30 : 27);
+    // const minWidth = this.props.team.get('score') > 10 ? 46 : (IOS ? 30 : 27);
 
-    this.setTimeout(() => {
-      LayoutAnimation.spring();
-      const percentageToTopscore = (this.props.team.get('score') / this.props.topscore) || 0;
-      const barWrapWidth = Dimensions.get('window').width -  120;// other content width
-      let barWidth = barWrapWidth * percentageToTopscore;
-      barWidth = Math.max(barWidth, minWidth); // minWidth for teams with low points
-      this.setState({ width: barWidth });
-    }, 1000);
+    // this.setTimeout(() => {
+    //   LayoutAnimation.spring();
+    //   const percentageToTopscore = (this.props.team.get('score') / this.props.topscore) || 0;
+    //   const barWrapWidth = Dimensions.get('window').width -  120;// other content width
+    //   let barWidth = barWrapWidth * percentageToTopscore;
+    //   barWidth = Math.max(barWidth, minWidth); // minWidth for teams with low points
+    //   this.setState({ width: barWidth });
+    // }, 1000);
   },
 
   render() {
-    const orderSuffix = this.getOrderSuffix(this.props.position);
+    const { position, last, logo, team } = this.props;
+    const orderSuffix = this.getOrderSuffix(position);
 
     return (
-      <View style={styles.entry}>
+      <View style={[styles.entry, last ? { marginBottom: 48 } : null]}>
 
         <View style={styles.entryLogo}>
           <Image
-          source={{ uri: this.props.logo }}
-          style={styles.entryLogoImg} />
+            source={{ uri: logo }}
+            style={styles.entryLogoImg} />
         </View>
 
         <View style={styles.entryContent}>
           <View style={styles.entryTitle}>
             <Text style={styles.entryTitleName}>
-            {this.props.team.get('name')}
+              {team.get('name')}
             </Text>
 
             <View style={styles.entryTitlePosition}>
               <Text style={styles.entryTitlePositionText}>
-                {this.props.position}{orderSuffix}
+                {position}{orderSuffix}
               </Text>
             </View>
-
           </View>
 
-          <View style={styles.barWrap}>
-
-            <View style={[
-              styles.bar,
-              {width: this.state.width }
-              ]} />
-
-            <Text style={[styles.entryTitleScore, styles.entryTitleScoreOver]}>
-            {this.props.team.get('score')}
+          <View style={styles.entryTitleScore}>
+            <Text style={[styles.entryTitleScoreText]}>
+              {team.get('score')}
             </Text>
-
           </View>
         </View>
-        <View style={styles.entryBottomLine} />
+        {!last && <View style={styles.entryBottomLine} />}
       </View>
     );
   }
@@ -107,37 +92,38 @@ const LeaderboardEntry = React.createClass({
 
 const styles = StyleSheet.create({
   entry: {
-    paddingTop:IOS ? 34 : 32,
-    paddingBottom:IOS ? 34 : 32,
-    backgroundColor:'#FFF',
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: theme.purpleLayer,
     flexDirection:'row',
     alignItems:'center'
   },
   entryBottomLine:{
     position:'absolute',
-    left:20,
-    right:20,
+    left: 30,
+    right: 30,
     bottom:0,
     height:1,
-    backgroundColor:'#f2f2f2',
+    backgroundColor: theme.white,
   },
   entryLogo:{
-    paddingLeft:20,
+    paddingLeft:30,
     paddingRight:0,
   },
   entryLogoImg:{
-    width:60,
-    height:60,
-    borderRadius:30,
-    borderWidth: 0,
-    borderColor: '#f3f3f3'
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    borderWidth: 5,
+    borderColor: theme.secondary,
   },
   entryContent: {
     flex:1,
     justifyContent:'space-between',
     flexDirection:'column',
-    paddingRight:20,
-    paddingLeft: 20,
+    paddingRight: 30,
+    paddingLeft: 10,
+    top: 6,
   },
   entryTitle: {
     justifyContent:'space-between',
@@ -149,50 +135,23 @@ const styles = StyleSheet.create({
   },
   entryTitlePositionText: {
     fontWeight:'normal',
-    color: theme.darkgrey,
-    fontSize:16,
+    color: theme.pink,
+    fontSize:18,
     flex:1,
     textAlign:'right'
   },
   entryTitleName:{
     fontSize:18,
     fontWeight:'normal',
+    color: theme.white,
   },
   entryTitleScore:{
     backgroundColor:'transparent',
-    color:'#ccc',
-    fontSize:18,
-    textAlign:'right',
   },
-  entryTitleScoreOver:{
-    position:'absolute',
-    left: 10,
-    top:IOS ? 7 : 3,
-    fontSize:14,
-    fontWeight:'bold',
-    color: '#fff',
-    //textShadowColor:theme.secondaryDark,
-    //textShadowOffset: {width: 0, height: 1},
-    //textShadowRadius: 1
+  entryTitleScoreText:{
+    fontSize: 36,
+    color: theme.accent,
   },
-  barWrap:{
-    backgroundColor:'transparent',
-    marginTop:3,
-    marginBottom:5,
-    overflow:'hidden',
-    height:IOS ? 30 : 26,
-    flex:1,
-  },
-  bar:{
-    height:IOS ? 30 : 26,
-    backgroundColor:theme.secondary,
-    position:'absolute',
-    borderRadius:30,
-    left:0,
-    top:0,
-    bottom:0,
-    width:0
-  }
 });
 
 export default LeaderboardEntry;
--- app/containers/CompetitionView.js
+++ app/containers/CompetitionView.js
@@ -8,12 +8,12 @@ import {
   View,
   Platform,
   ScrollView,
-  Text,
   TouchableOpacity,
   TouchableNativeFeedback,
   Image,
   RefreshControl
 } from 'react-native';
+import Text from '../components/Text';
 import Immutable from 'immutable';
 import { connect } from 'react-redux';
 
@@ -23,6 +23,7 @@ import analytics from '../services/analytics';
 import LeaderboardEntry from '../components/competition/LeaderboardEntry';
 const Icon = require('react-native-vector-icons/Ionicons');
 import ICONS from '../constants/Icons';
+import Background from '../components/background';
 
 
 const IOS = Platform.OS === 'ios';
@@ -47,30 +48,6 @@ const CompetitionView = React.createClass({
     this.props.dispatch(TeamActions.changeListType(type));
   },
 
-  renderTabs() {
-    const TABS = ['Team', 'Personal'];
-    const { leaderBoardFilter } = this.props;
-
-
-    return (
-      <View style={styles.tabsWrap }>
-        <View style={ styles.tabs }>
-          {TABS.map((tab, index) => {
-            const isCurrentTab = index === 0 && !leaderBoardFilter || leaderBoardFilter === tab;
-            return (
-            <PlatformTouchable activeOpacity={0.6} delayPressIn={1} onPress={() => this.changeLeaderBoardFilter(tab)} style={styles.tabLink} >
-              <View style={ [styles.tab, { backgroundColor: isCurrentTab ? theme.secondary : null }]}>
-                <Text style={ [styles.tabText, { color: isCurrentTab ? theme.white : theme.secondary }] }>{tab}</Text>
-              </View>
-            </PlatformTouchable>
-            )
-          })}
-        </View>
-      </View>
-
-    )
-  },
-
   render() {
     let topscore = 0;
     this.props.teams.map((team) => {
@@ -81,50 +58,34 @@ const CompetitionView = React.createClass({
     const refreshControl = <RefreshControl
       refreshing={this.props.isRefreshing}
       onRefresh={this.onRefreshFeed}
-      colors={[theme.secondaryLight]}
-      tintColor={theme.secondaryLight}
+      colors={[theme.accent]}
+      tintColor={theme.accent}
       progressBackgroundColor={theme.light} />;
 
     return (
       <View style={styles.container}>
-
-
-
+        <Background color="purple" />
 
         <View style={styles.leaderboardIntro}>
-          <View style={styles.leaderboardIconWrap}>
-            {/*<Icon name='trophy' style={styles.leaderboardIcon} /> */}
-            <Image source={ICONS.CROWN} style={styles.leaderboardIcon} />
-          </View>
           <View style={styles.leaderboardIntroTextWrap}>
             <Text style={styles.leaderboardIntroText}>
-                Current Futubileet2016 points for each team.
                 Be active App user and lead your team to the victory!
             </Text>
           </View>
         </View>
 
-
-        {/*this.renderTabs()}
-
-        {(!this.props.leaderBoardFilter || this.props.leaderBoardFilter === 'Team') ?
-        <ScrollView style={styles.leaderboard}
-          refreshControl={refreshControl}
-        >
-          {this.props.teams.map((team, index) =>
-            <LeaderboardEntry key={team.get('id')} topscore={+topscore}
-              team={team} position={index + 1} logo={team.get('imagePath')} />
-          )}
-        </ScrollView> :
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}><Text>Personal Leaderboard is coming later</Text></View>
-        */}
-
         <ScrollView style={styles.leaderboard}
         refreshControl={refreshControl}
         >
         {this.props.teams.map((team, index) =>
-          <LeaderboardEntry key={team.get('id')} topscore={+topscore}
-          team={team} position={index + 1} logo={team.get('imagePath')} />
+          <LeaderboardEntry
+            key={team.get('id')}
+            topscore={+topscore}
+            team={team}
+            position={index + 1}
+            logo={team.get('imagePath')}
+            last={this.props.teams.size === index + 1}
+          />
           )}
         </ScrollView>
 
@@ -137,21 +98,19 @@ const CompetitionView = React.createClass({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
+    backgroundColor: 'transparent',
     paddingBottom: 0,
   },
   leaderboardIntro:{
-    flexDirection:'row',
+    flexDirection: 'row',
     margin:20,
-    marginBottom:0,
-    marginTop:0,
+    marginBottom: 0,
+    marginTop: 15,
     padding: IOS ? 10 : 13,
     paddingLeft:7,
     paddingRight:5,
     justifyContent:'space-between',
     alignItems: 'center',
-    borderBottomWidth:2,
-    borderBottomColor:'#f2f2f2'
   },
   leaderboardIconWrap:{
     width: 72,
@@ -166,10 +125,13 @@ const styles = StyleSheet.create({
   },
   leaderboardIntroTextWrap:{
     flex:1,
+    backgroundColor: theme.pink,
+    padding: 15,
+    paddingBottom: 10,
   },
   leaderboardIntroText:{
-    color: theme.dark,
-    fontSize:12
+    color: theme.white,
+    fontSize: 18
   },
   leaderboardIntroText__grey:{
     color:'#aaa',
@@ -177,14 +139,9 @@ const styles = StyleSheet.create({
   },
   leaderboard: {
     flex: 1,
-    marginBottom: IOS ? 25 : 0,
+    marginBottom: IOS ? 0 : 0,
 
   },
-  tabsWrap: { margin: IOS ? 20 : 0, marginTop: IOS ? 10 : 0, marginBottom: IOS ? 10 : 0, elevation: 3 },
-  tabs: { borderRadius: IOS ? 5 : 0, borderWidth: IOS ? 1 : 0, borderTopWidth: 1, borderColor: IOS ? theme.secondary : '#f2f2f2', flex: 1, flexDirection: 'row' },
-  tabLink: { height: IOS ? 32 : 45, flex: 1 },
-  tab: { height: IOS ? 32 : 45, flex: 1, alignItems: 'center', justifyContent: 'center'},
-  tabText: { color: theme.secondary }
 
 });
 
