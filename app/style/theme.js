da1ccc0398052cbc216c1e1e40cff5a037049b6a
Pasi Lampinen <plam@fl027.local>
2017-09-07 12:54:49 +0300

    Updates theme colors

--- app/style/theme.js
+++ app/style/theme.js
@@ -6,29 +6,30 @@ const theme = {
   primary:        '#231F20',
   primaryDarker:  '#000', // This is used for Android Status bar (needs darker version of primary color) (also used for currently active iOS tab item)
   primaryDark:    '#111',
-  secondary:      '#FF5240',
-  secondaryLight: '#fc6355',
-  secondaryDark:  '#6b2019',
+
+  secondary:      '#44338D',
+  secondaryLight: '#4b3b93',
+  secondaryDark:  '#3a2c7f',
   secondaryBlur:  '#F1523F',
   secondaryStatus:'#de4b3c',
+
+
   statusbar:      '#e54838',
-  accent:         '#FFEE47',
-  accentLight:    '#FFF5AF',
+
+  accent:         '#FEF4B7',
+  accentLight:    '#FEF4B7',
+
+  pink:           '#ff9090',
+  purpleLayer:    'rgba(74, 48, 146, .8)',
+  transparent:    'transparent',
+
   danger:         '#FF5240',
   stable:         '#f6f6f6',
-  feed:           '#E5E4E9',
   lightblue:      '#EDF2F5',
   light:          '#fff',
   dark:           '#231F20',
   white:          '#fff',
-  darkgrey:       '#738998',
-  green:          '#349C4A',
-  greenAccent:    '#42AF47',
-  greenLight:     '#cff9d9',
-  greenDark:       '#003340',
   grey:           '#7D878C',
-  lightGrey:      '#ddd',
-  red:            '#FF5240',
   black:          '#000',
 };
 
