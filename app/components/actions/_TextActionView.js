91374c5b28d42af205949b7e64677fcc0b4cde17
Pasi Lampinen <plam@fl027.local>
2017-09-11 20:34:36 +0300

    TextActionView with react-native Modal component

--- app/components/actions/_TextActionView.js
+++ app/components/actions/_TextActionView.js
@@ -1,118 +1,99 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 
 import {
   View,
+  Text,
   TextInput,
   Platform,
   Dimensions,
   Animated,
   StyleSheet,
-  KeyboardAvoidingView,
+  BackAndroid,
   Modal
 } from 'react-native';
 import { connect } from 'react-redux';
-
-import Text from '../Text';
 import Button from '../../components/common/Button';
 import theme from '../../style/theme';
+// import Modal from 'react-native-modalbox';
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import {
-  postText,
-  closeTextActionView
-} from '../../actions/competition';
-
+import * as CompetitionActions from '../../actions/competition';
 const IOS = Platform.OS === 'ios';
 
 const { width, height } = Dimensions.get('window');
 
-class TextActionView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+
+const TextActionView = React.createClass({
+  propTypes: {
+    dispatch: PropTypes.func.isRequired,
+    isTextActionViewOpen: PropTypes.bool.isRequired
+  },
+  getInitialState() {
+    return {
       text: '',
       formAnimation: new Animated.Value(1),
       okAnimation: new Animated.Value(0)
-    };
-
-    this.onChangeText = this.onChangeText.bind(this);
-    this.onClose = this.onClose.bind(this);
-    this.onSendText = this.onSendText.bind(this);
-    this.hideOK = this.hideOK.bind(this);
-    this.showOK = this.showOK.bind(this);
-  }
-
-  componentWillReceiveProps({ isTextActionViewOpen }) {
-    // when opening again
-    if (isTextActionViewOpen && !this.props.isTextActionViewOpen){
-      this.hideOK();
-      this.setState({text: ''});
     }
-  }
-
+  },
+  componentDidMount(){
+    BackAndroid.addEventListener('hardwareBackPress', () => {
+      if (this.props.isTextActionViewOpen) {
+        this.onCancel()
+        return true;
+      }
+      return false;
+    })
+  },
   showOK() {
-    Animated.spring(this.state.okAnimation, { toValue: 1, duration: 250 }).start();
-    Animated.timing(this.state.formAnimation, { toValue: 0, duration: 100 }).start();
-  }
-
+    Animated.spring(this.state.okAnimation, {toValue:1, duration:250}).start();
+    Animated.timing(this.state.formAnimation, {toValue:0, duration:100}).start();
+  },
   hideOK() {
     this.state.formAnimation.setValue(1);
     this.state.okAnimation.setValue(0);
-  }
-
+  },
   onChangeText(text) {
     this.setState({text: text});
-  }
-
-  onClose() {
+  },
+  onCancel() {
     this.setState({text: ''});
-    this.props.closeTextActionView();
-  }
-
+    this.props.dispatch(CompetitionActions.closeTextActionView());
+  },
   onSendText() {
 
     if (!this.state.text.length) {
-      this.onClose();
+      this.onCancel();
       return;
     }
 
-    this.showOK();
-    this.props.postText(this.state.text);
-
+    this.showOK()
     setTimeout(() => {
-      this.props.postText(this.state.text);
-      this.setState({ text: '' });
-      this.props.closeTextActionView();
+      this.props.dispatch(CompetitionActions.postText(this.state.text));
+      this.setState({text: ''});
+      this.props.dispatch(CompetitionActions.closeTextActionView());
 
       // reset values for the next time
       setTimeout(() => {
         this.hideOK();
-      }, 100);
+      },100);
 
     }, 600);
-  }
 
+  },
   render() {
-
-    const { isTextActionViewOpen } = this.props;
-
-    if (!isTextActionViewOpen) {
-      return false;
-    }
-
     return (
       <Modal
         onRequestClose={this.onClose}
-        visible={isTextActionViewOpen}
+        visible={this.props.isTextActionViewOpen}
         animationType={'slide'}
       >
         <View style={[styles.container, styles.modalBackgroundStyle]}>
 
-          <Animated.View style={[styles.okView, { opacity: this.state.okAnimation }]}>
+          <Animated.View style={[styles.okView, { opacity: this.state.okAnimation}]}>
             <Animated.View style={[styles.okWrap,
-              { opacity: this.state.okAnimation, transform:[{ scale: this.state.okAnimation }] }
+              {opacity: this.state.okAnimation, transform:[{ scale: this.state.okAnimation }]}
             ]}>
               <Icon name='done' style={styles.okSign} />
             </Animated.View>
@@ -123,7 +104,7 @@ class TextActionView extends Component {
 
 
           <Animated.View style={[styles.innerContainer, {opacity:this.state.formAnimation}]}>
-          <KeyboardAvoidingView behavior={IOS ? 'position' : 'height'} keyboardVerticalOffset={IOS ? -100 : 30} style={styles.inputContainer}>
+
           {/*
             <View>
               <View style={styles.title}>
@@ -139,15 +120,12 @@ class TextActionView extends Component {
               underlineColorAndroid={'transparent'}
               clearButtonMode={'while-editing'}
               returnKeyType={'send'}
-              blurOnSubmit={true}
               onSubmitEditing={this.onSendText}
-              style={styles.inputField}
+              style={[styles.inputField, styles['inputField_' + Platform.OS]]}
               onChangeText={this.onChangeText}
               numberOfLines={3}
-              maxLength={151}
-              placeholderTextColor={'rgba(255,255,255, 0.65)'}
-              placeholder=" Say something..."
-              autoCorrect={false}
+              placeholderTextColor={'rgba(255,255,255, 0.5)'}
+              placeholder="Say something..."
               value={this.state.text} />
 
 
@@ -161,7 +139,7 @@ class TextActionView extends Component {
 
             <View style={styles.bottomButtons}>
               <Button
-                onPress={this.onClose}
+                onPress={this.onCancel}
                 style={styles.cancelButton}>
                 Cancel
               </Button>
@@ -169,37 +147,29 @@ class TextActionView extends Component {
               <Button
                 onPress={this.onSendText}
                 style={styles.modalButton}
-                textStyle={{ color: theme.white }}
                 isDisabled={!this.state.text}>
-                Post
+                Send
               </Button>
             </View>
-            </KeyboardAvoidingView>
           </Animated.View>
         </View>
       </Modal>
     );
   }
-}
+});
 
 // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 0,
-    paddingBottom: 0,
-    justifyContent: 'center'
+    paddingTop:0,
+    paddingBottom:IOS ? 49 : 0
   },
   innerContainer: {
-    padding: IOS ? 10 : 0,
-    paddingBottom: 10,
+    padding:10,
+    paddingBottom: 60,
     flex:1,
-    justifyContent: 'center',
-  },
-  inputContainer: {
-    backgroundColor: 'transparent',
-    justifyContent: 'center',
-    flex: 1,
+    justifyContent: 'center'
   },
   title:{
     padding: 10,
@@ -212,7 +182,7 @@ const styles = StyleSheet.create({
   titleText:{
     fontSize: 20,
     color: theme.primary,
-    fontWeight: 'normal',
+    fontWeight: 'bold',
     textAlign: IOS ? 'center' : 'left',
   },
   titleIcon:{
@@ -225,40 +195,42 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: IOS ? 'stretch' : 'flex-end',
     justifyContent: IOS ? 'center' : 'flex-end',
-    position: 'absolute',
-    bottom: IOS ? 0 : 10,
-    right: 0,
-    left: 0,
-    padding: 20,
-    paddingBottom: 0,
-    paddingLeft: IOS ? 0 : 20,
-    paddingRight: IOS ? 0 : 20,
+    position: IOS ? 'absolute' : 'absolute',
+    bottom:0,
+    right:0,
+    left:0,
+    padding:10,
+    paddingBottom: IOS ? 0 : 10,
+    paddingLeft:20,
+    paddingRight: 20,
     borderTopWidth: IOS ? 0 : 1,
     borderTopColor:'rgba(0,0,0,.1)',
   },
   modalButton: {
     flex: 1,
-    marginLeft: 6,
-    backgroundColor: theme.primary
+    marginLeft: 10,
   },
   cancelButton: {
     flex: 1,
-    marginRight: 6,
-    backgroundColor: '#aaa',
+    marginRight: 10,
+    backgroundColor: '#999',
   },
   modalBackgroundStyle: {
     backgroundColor: theme.secondary
   },
   inputField: {
-    fontFamily: 'Futurice',
     fontSize: 18,
     margin: 0,
-    marginLeft: 20,
-    marginTop: IOS ? 110 : 0,
     color:'#FFF',
     textAlign: 'center',
-    height: 220,
+    height: 150,
     width: width - 40,
+    fontFamily: 'Futurice',
+  },
+  inputField_android: {
+
+  },
+  inputField_ios: {
   },
   bottomInfo:{
     padding: 15,
@@ -273,7 +245,7 @@ const styles = StyleSheet.create({
   },
   okView: {
     position: 'absolute',
-    top: IOS ? height / 2 - 170 : 50,
+    top: IOS ? height / 2 - 140 : 50,
     left: 0,
     right: 0,
     alignItems: 'center',
@@ -283,36 +255,34 @@ const styles = StyleSheet.create({
   okWrap:{
     position: 'relative',
     overflow: 'visible',
+    borderWidth: 5,
+    borderColor: theme.light,
     paddingTop: 32,
+    borderRadius: 70,
     width: 140,
     height: 140,
     opacity: 0,
-    transform: [{ scale: 0 }]
+    transform: [{scale: 0}]
   },
   okSign:{
-    fontSize: 105,
+    fontSize: 65,
     color: theme.light,
     backgroundColor: 'transparent',
     textAlign: 'center',
   },
   okText:{
     color: theme.light,
-    fontWeight: 'normal',
+    fontWeight: 'bold',
     textAlign: 'center',
     backgroundColor: 'transparent',
     fontSize: 15
   }
 });
 
-
-const mapDispatchToProps = {
-  closeTextActionView,
-  postText
+const select = store => {
+  return {
+    isTextActionViewOpen: store.competition.get('isTextActionViewOpen')
+  };
 };
 
-
-const select = store => ({
-  isTextActionViewOpen: store.competition.get('isTextActionViewOpen')
-})
-
-export default connect(select, mapDispatchToProps)(TextActionView);
+export default connect(select)(TextActionView);
