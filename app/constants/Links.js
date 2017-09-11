5763ac11632d93288c573607cb23cb50d403673e
Pasi Lampinen <plam@fl027.local>
2017-09-11 20:36:43 +0300

    Update Links and Terms

--- app/constants/Links.js
+++ app/constants/Links.js
@@ -1,17 +1,16 @@
-const ROOT_URL = 'https://futurice.github.io/futubileet-site';
-
-
+import TermsView from '../components/Terms';
 import locationService from '../services/location';
+
+const ROOT_URL = 'https://futurice.github.io/futubileet-site';
 const EVENT_LOCATION = { latitude: 60.1756973, longitude: 24.9336035 };
 
 const Links = [
   {title: 'Finlandia Hall Website', link: 'https://www.finlandiatalo.fi/en', icon: 'account-balance'},
-  {title: 'Directions to Heureka', link: locationService.getGeoUrl({ location: EVENT_LOCATION, locationName: 'Futubileet in Heureka' }), icon: 'directions' },
+  {title: 'Directions to Finlandia Hall', link: locationService.getGeoUrl({ location: EVENT_LOCATION, locationName: 'Futufinlandia' }), icon: 'directions' },
   {title: 'Call Taxi Helsinki', link: 'tel://01000700', icon: 'directions-car', separatorAfter: false},
-  {title: 'Terms of Service', link: `${ROOT_URL}/terms.html`, icon: 'info', secondary: true, showInWebview: true},
+  {title: 'Terms of Service', component: TermsView, icon: 'info', secondary: true, showInWebview: true},
   {title: 'Privacy', link: `${ROOT_URL}/privacy.html`, icon: 'lock', secondary: true, showInWebview: true},
   {title: 'Licenses', link: `${ROOT_URL}/licenses.html`, icon: 'help', secondary: true, showInWebview: true, last: true},
-
 ];
 
 
